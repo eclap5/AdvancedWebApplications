9f5c7a529f394818c43d3dd154f6c594663ac45f
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2023-11-21 01:09:19 +0200

    Week 4 all excercises finished

---
+++ Week4/routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Recipe App' });
+});
+
+router.get('/recipe/:food', (req, res, next) => {
+  res.json(
+    { 
+      name: req.params.food,
+      instructions: ['step 1', 'step 2', 'step 3'],
+      ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'] 
+    }
+  )
+})
+
+router.post('/recipe/', (req, res, next) => {
+  recipes.push(req.body)
+  res.json(req.body)
+})
+
+router.post('/images', (req, res, next) => {
+  res.send('POST successful')
+})
+
+module.exports = router;
